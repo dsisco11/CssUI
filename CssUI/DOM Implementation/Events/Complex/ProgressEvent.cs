945924254bac10e0b24120a0f1854b7fe45f1936
Sisco <dsisco11@gmail.com>
2019-07-24 19:45:06 -0500

    Adding ProgressEvent type

---
+++ CssUI/DOM Implementation/Events/Complex/ProgressEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace CssUI.DOM.Events
+{
+    public class ProgressEvent : Event
+    {/* Docs: https://xhr.spec.whatwg.org/#progressevent */
+        #region Properties
+        public readonly bool lengthComputable;
+        public readonly ulong loaded;
+        public readonly ulong total;
+        #endregion
+
+        #region Constructors
+        public ProgressEvent(EventName type, ProgressEventInit initDict = null) : base(type, initDict)
+        {
+            lengthComputable = initDict.lengthComputable;
+            loaded = initDict.loaded;
+            total = initDict.total;
+        }
+        #endregion
+
+    }
+}
---
+++ CssUI/DOM Implementation/Events/Option Structs/ProgressEventInit.cs
@@ -0,0 +1,22 @@
+﻿namespace CssUI.DOM.Events
+{
+    public class ProgressEventInit : EventInit
+    {
+        #region Properties
+        public readonly bool lengthComputable;
+        public readonly ulong loaded;
+        public readonly ulong total;
+        #endregion
+
+        #region Constructor
+        public ProgressEventInit(bool lengthComputable, ulong loaded, ulong total)
+        {
+            this.lengthComputable = lengthComputable;
+            this.loaded = loaded;
+            this.total = total;
+        }
+        #endregion
+
+
+    }
+}
