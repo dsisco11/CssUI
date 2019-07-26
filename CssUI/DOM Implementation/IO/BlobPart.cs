1195c6e1ab350e9f03a624df9762e4f8297b868e
Sisco <dsisco11@gmail.com>
2019-07-26 07:34:03 -0500

    Implemented HTML IO types

---
+++ CssUI/DOM Implementation/IO/BlobPart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CssUI.DOM
+{
+    public class BlobPart
+    {
+        #region Properties
+        public readonly dynamic data;
+        #endregion
+
+
+        #region Constructors
+        public BlobPart(ReadOnlyMemory<byte> data)
+        {
+            this.data = data;
+        }
+        public BlobPart(Blob data)
+        {
+            this.data = data;
+        }
+        public BlobPart(string data)
+        {
+            this.data = data;
+        }
+        #endregion
+
+        #region Implicit
+        public static implicit operator BlobPart(ReadOnlyMemory<byte> data) => new BlobPart(data);
+        public static implicit operator BlobPart(Blob blob) => new BlobPart(blob);
+        public static implicit operator BlobPart(string str) => new BlobPart(str);
+        #endregion
+    }
+}
