c2af88294409367db3fae930b146da263c7278d4
Sisco <dsisco11@gmail.com>
2019-07-29 16:57:39 -0500

    Add interface for browsing context containers (eg: iframe)

---
+++ CssUI/DOM Implementation/Interfaces/IBrowsingContextContainer.cs
@@ -0,0 +1,9 @@
+﻿using CssUI.DOM.Internal;
+
+namespace CssUI.DOM
+{
+    public interface IBrowsingContextContainer
+    {/* Docs: https://html.spec.whatwg.org/multipage/browsers.html#browsing-context-container */
+        BrowsingContext Nested_Browsing_Context { get; }
+    }
+}
