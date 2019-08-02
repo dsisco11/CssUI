2e7267c761564538b7ef64300b60b8a45e08dbb4
Sisco <dsisco11@gmail.com>
2019-08-02 15:58:59 -0500

    Create ReadOnlyColor.cs
    Adding NaN static instance decleration

---
+++ CssUI/Structs/ReadOnlyColor.cs
@@ -0,0 +1,27 @@
+﻿namespace CssUI
+{
+    public struct ReadOnlyColor
+    {
+        #region Instances
+        public static ReadOnlyColor NaN = new ReadOnlyColor(double.NaN, double.NaN, double.NaN, double.NaN);
+        #endregion
+
+
+        #region Properties
+        public readonly double R;
+        public readonly double G;
+        public readonly double B;
+        public readonly double A;
+        #endregion
+
+        #region Constructors
+        public ReadOnlyColor(double r, double g, double b, double a)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+        #endregion
+    }
+}
