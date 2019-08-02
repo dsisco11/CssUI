892fcb74f7beda3250f7abbd5fae8adca6077f98
Sisco <dsisco11@gmail.com>
2019-08-02 16:56:03 -0500

    Update SimpleColor.cs
    Simple colors dont store alpha, whoops

--- CssUI/Structs/SimpleColor.cs
+++ CssUI/Structs/SimpleColor.cs
@@ -1,30 +1,31 @@
-﻿namespace CssUI
+﻿using System.Runtime.InteropServices;
+
+namespace CssUI
 {
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct SimpleColor
     {
         #region Instances
-        public readonly static SimpleColor MinValue = new SimpleColor(byte.MinValue, byte.MinValue, byte.MinValue, byte.MinValue);
-        public readonly static SimpleColor MaxValue = new SimpleColor(byte.MaxValue, byte.MaxValue, byte.MaxValue, byte.MaxValue);
+        public readonly static SimpleColor MinValue = new SimpleColor(byte.MinValue, byte.MinValue, byte.MinValue);
+        public readonly static SimpleColor MaxValue = new SimpleColor(byte.MaxValue, byte.MaxValue, byte.MaxValue);
         #endregion
 
         #region Properties
-        public byte R, G, B, A;
+        public byte R, G, B;
         #endregion
 
         #region Constructors
-        public SimpleColor(byte r, byte g, byte b, byte a)
+        public SimpleColor(byte r, byte g, byte b)
         {
             R = r;
             G = g;
             B = b;
-            A = a;
         }
-        public SimpleColor(int r, int g, int b, int a)
+        public SimpleColor(int r, int g, int b)
         {
             R = (byte)r;
             G = (byte)g;
             B = (byte)b;
-            A = (byte)a;
         }
         #endregion
     }
