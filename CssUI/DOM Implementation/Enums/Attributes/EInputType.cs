ee237d36471731de0873f65e98c1b0aff38995b5
Sisco <dsisco11@gmail.com>
2019-08-03 14:27:34 -0500

    Update EInputType.cs
    Move to meta enum table

--- CssUI/DOM Implementation/Enums/Attributes/EInputType.cs
+++ CssUI/DOM Implementation/Enums/Attributes/EInputType.cs
@@ -1,144 +1,144 @@
-﻿using CssUI.DOM.Internal;
+﻿using CssUI.Internal;
 
 namespace CssUI.DOM
 {
     /// <summary>
     /// The type attribute controls the data type (and associated control) of an input element.
     /// </summary>
-    [DomEnum]
+    [MetaEnum]
     public enum EInputType : int
     {/* Docs: https://html.spec.whatwg.org/multipage/input.html#attr-input-type */
 
         /// <summary>
         /// Arbitrart string
         /// </summary>
-        [DomKeyword("hidden")]
+        [MetaKeyword("hidden")]
         Hidden,
 
         /// <summary>
         /// Text with no line breaks
         /// </summary>
-        [DomKeyword("text")]
+        [MetaKeyword("text")]
         Text,
 
         /// <summary>
         /// Text with no line breaks
         /// </summary>
-        [DomKeyword("search")]
+        [MetaKeyword("search")]
         Search,
 
         /// <summary>
         /// Text with no line breaks
         /// </summary>
-        [DomKeyword("tel")]
+        [MetaKeyword("tel")]
         Telephone,
 
         /// <summary>
         /// An absolute URL
         /// </summary>
-        [DomKeyword("url")]
+        [MetaKeyword("url")]
         Url,
 
         /// <summary>
         /// An e-mail address or list of e-mail addresses
         /// </summary>
-        [DomKeyword("email")]
+        [MetaKeyword("email")]
         Email,
 
         /// <summary>
         /// Text with no line breaks (sensitive information)
         /// </summary>
-        [DomKeyword("password")]
+        [MetaKeyword("password")]
         Password,
 
         /// <summary>
         /// A date (year, month, day) with no time zone
         /// </summary>
-        [DomKeyword("date")]
+        [MetaKeyword("date")]
         Date,
 
         /// <summary>
         /// A date consisting of a year and a month with no time zone
         /// </summary>
-        [DomKeyword("month")]
+        [MetaKeyword("month")]
         Month,
 
         /// <summary>
         /// A date consisting of a week-year number and a week number with no time zone
         /// </summary>
-        [DomKeyword("week")]
+        [MetaKeyword("week")]
         Week,
 
         /// <summary>
         /// A time (hour, minute, seconds, fractional seconds) with no time zone
         /// </summary>
-        [DomKeyword("time")]
+        [MetaKeyword("time")]
         Time,
 
         /// <summary>
         /// A date and time (year, month, day, hour, minute, second, fraction of a second) with no time zone
         /// </summary>
-        [DomKeyword("datetime-local")]
+        [MetaKeyword("datetime-local")]
         Local,
 
         /// <summary>
         /// A numerical value
         /// </summary>
-        [DomKeyword("number")]
+        [MetaKeyword("number")]
         Number,
 
         /// <summary>
         /// A numerical value, with the extra semantic that the exact value is not important
         /// </summary>
-        [DomKeyword("range")]
+        [MetaKeyword("range")]
         Range,
 
         /// <summary>
         /// An sRGB color with 8-bit red, green, and blue components
         /// </summary>
-        [DomKeyword("color")]
+        [MetaKeyword("color")]
         Color,
 
         /// <summary>
         /// A set of zero or more values from a predefined list
         /// </summary>
-        [DomKeyword("checkbox")]
+        [MetaKeyword("checkbox")]
         Checkbox,
 
         /// <summary>
         /// An enumerated value
         /// </summary>
-        [DomKeyword("radio")]
+        [MetaKeyword("radio")]
         Radio,
 
         /// <summary>
         /// Zero or more files each with a MIME type and optionally a file name
         /// </summary>
-        [DomKeyword("file")]
+        [MetaKeyword("file")]
         File,
 
         /// <summary>
         /// An enumerated value, with the extra semantic that it must be the last value selected and initiates form submission
         /// </summary>
-        [DomKeyword("submit")]
+        [MetaKeyword("submit")]
         Submit,
 
         /// <summary>
         /// A coordinate, relative to a particular image's size, with the extra semantic that it must be the last value selected and initiates form submission
         /// </summary>
-        [DomKeyword("image")]
+        [MetaKeyword("image")]
         Image,
 
         /// <summary>
         /// n/a
         /// </summary>
-        [DomKeyword("reset")]
+        [MetaKeyword("reset")]
         Reset,
 
         /// <summary>
         /// n/a
         /// </summary>
-        [DomKeyword("button")]
+        [MetaKeyword("button")]
         Button,
     }
 }
