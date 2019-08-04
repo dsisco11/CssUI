a4badb6913d1f12f8bc439d3630878a2b1ace6c4
Sisco <dsisco11@gmail.com>
2019-08-04 00:58:57 -0500

    Reorganize DataFilters
    Also added new FilterHTMLWhitespace filter

---
+++ CssUI/Data Filters/Complex/HTML/FilterHTMLWhitespace.cs
@@ -0,0 +1,24 @@
+﻿using CssUI.HTML;
+using System.Runtime.CompilerServices;
+
+namespace CssUI.Filters
+{
+    /// <summary>
+    /// Skips ASCII whitespace chars
+    /// </summary>
+    public class FilterHTMLWhitespace : DataFilter<char>
+    {
+        public static DataFilter<char> Instance = new FilterHTMLWhitespace();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override EFilterResult acceptData(char data)
+        {
+            if (HTMLCommon.Is_HTML_Whitespace(data))
+            {
+                return EFilterResult.FILTER_SKIP;
+            }
+
+            return EFilterResult.FILTER_ACCEPT;
+        }
+    }
+}
