ac7a1efa75ea18ac1dc714011b471f1856612c64
Sisco <dsisco11@gmail.com>
2019-08-03 19:47:01 -0500

    Update ISubmittableElement.cs
    submittable elements ARE form related so they should inherit from the listed element interface

--- CssUI/DOM Implementation/Interfaces/ISubmittableElement.cs
+++ CssUI/DOM Implementation/Interfaces/ISubmittableElement.cs
@@ -1,11 +1,9 @@
-﻿using CssUI.DOM.Events;
-
-namespace CssUI.DOM
+﻿namespace CssUI.DOM
 {
     /// <summary>
     /// Denotes elements that can be used for constructing the entry list when a form element is submitted.
     /// </summary>
-    public interface ISubmittableElement : IEventTarget
+    public interface ISubmittableElement : IListedElement
     {
     }
 }
