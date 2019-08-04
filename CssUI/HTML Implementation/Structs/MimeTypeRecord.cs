b8e126ee1ea5202307845fd0bf6ddded6c0a1136
Sisco <dsisco11@gmail.com>
2019-08-04 01:42:51 -0500

    Create MimeTypeRecord.cs

---
+++ CssUI/HTML Implementation/Structs/MimeTypeRecord.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CssUI.HTML
+{
+    public struct MimeTypeRecord
+    {
+        #region Properties
+        public readonly string Type;
+        public readonly string SubType;
+        public readonly Dictionary<string, string> Parameters;
+        #endregion
+
+        #region Constructors
+        public MimeTypeRecord(string type, string subType, Dictionary<string, string> parameters)
+        {
+            Type = type;
+            SubType = subType;
+            Parameters = parameters;
+        }
+        #endregion
+    }
+}
