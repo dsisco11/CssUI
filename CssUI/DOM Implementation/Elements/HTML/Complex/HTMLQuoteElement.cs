f437ce30ad29a560ff34d2e61c0e4d9f84cecf7a
Sisco <dsisco11@gmail.com>
2019-08-03 20:31:57 -0500

    Implement HTMLQuoteElement

--- CssUI/DOM Implementation/Elements/HTML/Complex/HTMLQuoteElement.cs
+++ CssUI/DOM Implementation/Elements/HTML/Complex/HTMLQuoteElement.cs
@@ -1,7 +1,28 @@
-﻿namespace CssUI.DOM
+﻿using CssUI.DOM.CustomElements;
+
+namespace CssUI.DOM
 {
     public class HTMLQuoteElement : HTMLElement
-    {/* Docs:  */
-        public string cite;
+    {/* Docs: https://html.spec.whatwg.org/multipage/grouping-content.html#the-blockquote-element */
+        #region Properties
+        #endregion
+
+        #region Constructors
+        public HTMLQuoteElement(Document document) : this(document, "quote")
+        {
+        }
+
+        public HTMLQuoteElement(Document document, string localName) : base(document, localName)
+        {
+        }
+        #endregion
+
+        #region Content Attributes
+        [CEReactions] public string cite
+        {
+            get => getAttribute(EAttributeName.Cite).Get_String();
+            set => CEReactions.Wrap_CEReaction(this, () => setAttribute(EAttributeName.Cite, AttributeValue.From_String(value)));
+        }
+        #endregion
     }
 }
