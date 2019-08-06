05092aa8e33d9e6ae9d5c7807a442f1521796e13
Sisco <dsisco11@gmail.com>
2019-08-06 16:11:01 -0500

    Update CSSPseudoElement.cs

--- CssUI/DOM Implementation/Elements/CSS/CSSPseudoElement.cs
+++ CssUI/DOM Implementation/Elements/CSS/CSSPseudoElement.cs
@@ -1,17 +1,11 @@
 ﻿using CssUI.CSS.Enums;
 using CssUI.DOM.Events;
-using CssUI.DOM.Geometry;
-using CssUI.DOM.Interfaces;
-using CssUI.DOM.Internal;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace CssUI.DOM
 {
     public class CSSPseudoElement : EventTarget
     {
         #region Properties
-        /* XXX: Replace with an AtomicName (need an enum) */
         public readonly AtomicName<EPseudoElement> type;
         public readonly Element element;
         #endregion
