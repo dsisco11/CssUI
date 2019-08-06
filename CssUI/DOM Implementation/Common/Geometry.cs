65abeb9dbb3a8b0362a646146ab3150510c872bd
Sisco <dsisco11@gmail.com>
2019-08-06 16:05:10 -0500

    Create Geometry.cs

---
+++ CssUI/DOM Implementation/Common/Geometry.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CssUI.DOM.Geometry
+{
+    public static class Geometry
+    {
+
+        public static bool Point_Within_Rect(DOMPointReadOnly point, DOMRectReadOnly rect)
+        {
+            return (point.x >= rect.left) && (point.x <= rect.right) && (point.y >= rect.top) && (point.y <= rect.bottom);
+        }
+    }
+}
