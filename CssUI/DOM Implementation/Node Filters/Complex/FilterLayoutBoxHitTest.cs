f08e5d59d3bdd8cc082ec39d35c594a6f273c441
Sisco <dsisco11@gmail.com>
2019-08-06 15:46:53 -0500

    Create FilterLayoutBoxHitTest.cs
    For use with pointer event hit testing

---
+++ CssUI/DOM Implementation/Node Filters/Complex/FilterLayoutBoxHitTest.cs
@@ -0,0 +1,34 @@
+﻿using CssUI.DOM.Enums;
+using CssUI.DOM.Nodes;
+using System;
+
+namespace CssUI.DOM
+{
+    /// <summary>
+    /// Rejects any nodes which do not descend from the specified Node
+    /// </summary>
+    public class FilterLayoutBoxHitTest : NodeFilter
+    {
+        private readonly double x, y;
+        public FilterLayoutBoxHitTest(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public override ENodeFilterResult acceptNode(Node node)
+        {
+            if (node.nodeType == ENodeType.ELEMENT_NODE && node is Element element)
+            {
+                if (element.Box == null) return ENodeFilterResult.FILTER_REJECT;
+                if (!element.Box.HitTest(x, y)) return ENodeFilterResult.FILTER_REJECT;
+
+                return ENodeFilterResult.FILTER_ACCEPT;
+            }
+
+            return ENodeFilterResult.FILTER_REJECT;
+        }
+
+    }
+}
