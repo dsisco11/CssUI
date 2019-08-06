9cc6083011755e1b003df4b39292fff77e2a5680
Sisco <dsisco11@gmail.com>
2019-08-06 16:05:58 -0500

    Update DOMRect.cs
    DOMRect should inherit from its read-only variant

--- CssUI/DOM Implementation/Geometry/Types/DOMRect.cs
+++ CssUI/DOM Implementation/Geometry/Types/DOMRect.cs
@@ -1,45 +1,45 @@
 ﻿namespace CssUI.DOM.Geometry
 {
-    public class DOMRect
+    public class DOMRect : DOMRectReadOnly
     {/* Docs: https://www.w3.org/TR/geometry-1/#DOMRect */
 
         #region Properties
-        public double x { get; set; }
-        public double y { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
+        public new double x { get; set; }
+        public new double y { get; set; }
+        public new double width { get; set; }
+        public new double height { get; set; }
         #endregion
 
         #region Accessors
-        public double top
-        {/* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-top */
+        /*public double top
+        {*//* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-top *//*
             get => MathExt.Min(y, y + height);
         }
         public double right
-        {/* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-right */
+        {*//* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-right *//*
             get => MathExt.Max(x, x + width);
         }
         public double bottom
-        {/* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-bottom */
+        {*//* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-bottom *//*
             get => MathExt.Max(y, y + height);
         }
         public double left
-        {/* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-left */
+        {*//* Docs: https://www.w3.org/TR/geometry-1/#dom-domrectreadonly-domrect-left *//*
             get => MathExt.Min(x, x + width);
-        }
+        }*/
         #endregion
 
         #region Constructor
-        public DOMRect(double x = 0, double y = 0, double width = 0, double height = 0)
+        public DOMRect(double x = 0, double y = 0, double width = 0, double height = 0) : base(x, y, width, height)
         {
-            this.x = x;
+            /*this.x = x;
             this.y = y;
             this.width = width;
-            this.height = height;
+            this.height = height;*/
         }
         #endregion
 
-        public static DOMRect fromRect(DOMRectInit other)
+        public new static DOMRect fromRect(DOMRectInit other)
         {
             return new DOMRect(other.x, other.y, other.width, other.height);
         }
