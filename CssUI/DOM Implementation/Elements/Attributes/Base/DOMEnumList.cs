ddacc681066d43a533141f232c958631c5677aad
Sisco <dsisco11@gmail.com>
2019-08-08 20:39:03 -0500

    Create DOMEnumList.cs
    Allows managing enumerated attributes that are ordered sets

---
+++ CssUI/DOM Implementation/Elements/Attributes/Base/DOMEnumList.cs
@@ -0,0 +1,38 @@
+﻿using CssUI.DOM.CustomElements;
+using CssUI.DOM.Exceptions;
+using CssUI.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace CssUI.DOM
+{
+    public class DOMEnumList<EnumType> : AttributeTokenList<EnumType> where EnumType : struct
+    {
+
+        #region Constructor
+        public DOMEnumList(Element ownerElement, AtomicName<EAttributeName> localName) : base(ownerElement, localName)
+        {
+        }
+
+        public DOMEnumList(Element ownerElement, AtomicName<EAttributeName> localName, EnumType[] supportedTokens) : base(ownerElement, localName, supportedTokens)
+        {
+        }
+
+        protected override EnumType String_To_Token(ReadOnlyMemory<char> str)
+        {
+            return Lookup.Enum<EnumType>(str);
+        }
+
+        protected override string Token_To_String(EnumType token)
+        {
+            return Lookup.Keyword(token);
+        }
+        #endregion
+
+
+
+    }
+}
