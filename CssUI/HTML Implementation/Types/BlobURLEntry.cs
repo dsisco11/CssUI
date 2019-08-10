eb5d38beeb2622bcfb52317f8f77f3e37a23b9b3
Sisco <dsisco11@gmail.com>
2019-08-09 20:40:12 -0500

    Create BlobURLEntry.cs

---
+++ CssUI/HTML Implementation/Types/BlobURLEntry.cs
@@ -0,0 +1,20 @@
+﻿namespace CssUI.HTML
+{
+    public class BlobURLEntry
+    {
+        #region Properties
+        public readonly dynamic Value;
+        #endregion
+
+        #region Constructors
+        public BlobURLEntry(Blob Value)
+        {
+            this.Value = Value;
+        }
+        #endregion
+
+        #region Implicit Casts
+        public static implicit operator BlobURLEntry(Blob blob) => new BlobURLEntry(blob);
+        #endregion
+    }
+}
