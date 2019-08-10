2146bd88648d61ee420e9c516367464e110d65cd
Sisco <dsisco11@gmail.com>
2019-08-10 03:09:09 -0500

    Create EUrlScheme.cs
    Common HTTP Url schemes

---
+++ CssUI/HTML Implementation/Enums/EUrlScheme.cs
@@ -0,0 +1,30 @@
+﻿using CssUI.Internal;
+
+namespace CssUI.HTML
+{
+    [MetaEnum]
+    public enum EUrlScheme : int
+    {/* Docs: https://url.spec.whatwg.org/#special-scheme */
+
+        [MetaKeyword("ftp", 21)]
+        Ftp,
+
+        [MetaKeyword("file")]
+        File,
+
+        [MetaKeyword("gopher", 70)]
+        Gopher,
+
+        [MetaKeyword("http", 80)]
+        Http,
+
+        [MetaKeyword("https", 443)]
+        Https,
+
+        [MetaKeyword("ws", 80)]
+        Ws,
+
+        [MetaKeyword("wss", 443)]
+        Wss,
+    }
+}
