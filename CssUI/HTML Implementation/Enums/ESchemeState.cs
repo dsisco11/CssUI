dd891642e63b8f841247a2f1419630c52195f0bb
Sisco <dsisco11@gmail.com>
2019-08-10 03:09:43 -0500

    Create ESchemeState.cs
    HTTP url scheme states used internally by the url parsing algorithm

---
+++ CssUI/HTML Implementation/Enums/ESchemeState.cs
@@ -0,0 +1,29 @@
+﻿namespace CssUI.HTML
+{
+    public enum ESchemeState : int
+    {/* Docs: https://url.spec.whatwg.org/#scheme-start-state */
+
+        None = 0,
+        SchemeStart = 1,
+        Scheme,
+        NoScheme,
+        SpecialRelativeOrAuthority,
+        PathOrAuthority,
+        Relative,
+        RelativeSlash,
+        SpecialAuthoritySlashes,
+        SpecialAuthorityIgnoreSlashes,
+        Authority,
+        Hostname,
+        Port,
+        File,
+        FileSlash,
+        FileHost,
+        PathStart,
+        Path,
+        CannotBeBaseURLPath,
+        Query,
+        Fragment,
+
+    }
+}
