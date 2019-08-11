f530b805e22c2fe8ea50011b47bfd800ac40aa01
Sisco <dsisco11@gmail.com>
2019-08-11 02:30:29 -0500

    Adding IPV4/IPV6 address structures

---
+++ CssUI/HTML Implementation/Structs/IPV4Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CssUI.HTML
+{
+    /// <summary>
+    /// Contains 8-bit integers making up a single 32-bit integer which indicates a network address
+    /// </summary>
+    public struct IPV4Address
+    {/* Docs: https://url.spec.whatwg.org/#concept-ipv4 */
+
+        #region Properties
+        public readonly byte[] Part;
+        #endregion
+
+        #region Constructors
+
+        public IPV4Address(byte A, byte B, byte C, byte D)
+        {
+            Part = new byte[4] { A, B, C, D };
+        }
+        #endregion
+
+        public UInt32 Address
+        {
+            get => (UInt32)((Part[0] << 0) | (Part[1] << 8) | (Part[2] << 16) | (Part[3] << 24));
+        }
+
+    }
+}
---
+++ CssUI/HTML Implementation/Structs/IPV6Address.cs
@@ -0,0 +1,18 @@
+﻿namespace CssUI.HTML
+{
+    public struct IPV6Address
+    {/* Docs: https://url.spec.whatwg.org/#concept-ipv6 */
+
+        #region Properties
+        public readonly ushort[] Parts;
+        #endregion
+
+        #region Constructors
+
+        public IPV6Address(ushort A, ushort B, ushort C, ushort D, ushort E, ushort F, ushort G, ushort H)
+        {
+            Parts = new ushort[8] { A, B, C, D, E, F, G, H };
+        }
+        #endregion
+    }
+}
