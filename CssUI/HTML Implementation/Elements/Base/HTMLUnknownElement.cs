ac3dcf5c2bf1b2f8c2f2fd9185687999c91cf3d0
Sisco <dsisco11@gmail.com>
2019-08-13 19:46:59 -0500

    Update HTMLUnknownElement.cs
    Adding categories overload

--- CssUI/HTML Implementation/Elements/Base/HTMLUnknownElement.cs
+++ CssUI/HTML Implementation/Elements/Base/HTMLUnknownElement.cs
@@ -4,9 +4,12 @@ namespace CssUI.HTML
 {
     public sealed class HTMLUnknownElement : HTMLElement
     {
+        public override EContentCategories Categories => EContentCategories.None;
 
+        #region Constructors
         public HTMLUnknownElement(Document document, string localName, string prefix, string Namespace) : base(document, localName)
         {
         }
+        #endregion
     }
 }
