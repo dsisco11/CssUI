f1b42ba1283b9b4ef89185b6eb980bfdddb170eb
Sisco <dsisco11@gmail.com>
2019-08-14 21:12:39 -0500

    Create EPointerButton.cs

---
+++ CssUI/Devices/Pointer/Enums/EPointerButton.cs
@@ -0,0 +1,12 @@
+﻿namespace CssUI.Devices
+{
+    public enum EPointerButton : short
+    {
+        Left = 0,
+        Middle = 1,
+        Right = 2,
+        X1 = 3,
+        X2 = 4,
+        Eraser = 5,
+    }
+}
