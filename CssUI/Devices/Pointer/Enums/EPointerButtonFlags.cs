aab0d3482facb597a44bf8ae2c371857d6820f29
Sisco <dsisco11@gmail.com>
2019-08-14 21:12:50 -0500

    Create EPointerButtonFlags.cs

---
+++ CssUI/Devices/Pointer/Enums/EPointerButtonFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CssUI.Devices
+{
+    [Flags]
+    public enum EPointerButtonFlags : short
+    {
+        None = 0x0,
+        Left = (1 << 1), // 1
+        Right = (1 << 2), // 2
+        Middle = (1 << 3), // 4
+        X1 = (1 << 4), // 8
+        X2 = (1 << 5), // 16
+        Eraser = (1 << 6), // 32
+    }
+}
