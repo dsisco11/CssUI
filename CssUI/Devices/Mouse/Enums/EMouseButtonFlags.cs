a2940938a90281fa92d50ada60135792d520da57
Sisco <dsisco11@gmail.com>
2019-08-14 21:12:25 -0500

    Create EMouseButtonFlags.cs

---
+++ CssUI/Devices/Mouse/Enums/EMouseButtonFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CssUI.Devices
+{
+    [Flags]
+    public enum EMouseButtonFlags : ushort
+    {
+        None = 0x0,
+        Left = 1 << 1,
+        Right = 1 << 2,
+        Middle = 1 << 3,
+        X1 = 1 << 4,
+        X2 = 1 << 5,
+    }
+}
