561ddf26fedfd811c882bb895b7742fc7025c6ea
Sisco <dsisco11@gmail.com>
2019-08-14 21:12:43 -0500

    Create EPointerType.cs

---
+++ CssUI/Devices/Pointer/Enums/EPointerType.cs
@@ -0,0 +1,15 @@
+﻿using CssUI.Internal;
+
+namespace CssUI.Devices
+{
+    [MetaEnum]
+    public enum EPointerType : short
+    {/* Docs: https://w3c.github.io/pointerevents/#pointerevent-interface */
+        [MetaKeyword("mouse")]
+        Mouse,
+        [MetaKeyword("pen")]
+        PenStylus,
+        [MetaKeyword("touch")]
+        TouchContact,
+    }
+}
