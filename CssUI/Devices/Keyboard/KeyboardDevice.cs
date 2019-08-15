8259065fccc2d6bfeb7090dd84041ce92ce82c54
Sisco <dsisco11@gmail.com>
2019-08-14 21:09:55 -0500

    Update KeyboardDevice.cs
    Add IsDown function

--- CssUI/Devices/Keyboard/KeyboardDevice.cs
+++ CssUI/Devices/Keyboard/KeyboardDevice.cs
@@ -1,13 +1,16 @@
-﻿
-using CssUI.DOM.Events;
-
-namespace CssUI.Devices
+﻿namespace CssUI.Devices
 {
     /// <summary>
     /// Represents the systems keyboard device
     /// </summary>
     public abstract class KeyboardDevice
     {
+        /// <summary>
+        /// Returns true if the given <paramref name="key"/> on the systems current keyboard device is currently in the 'down' state
+        /// </summary>
+        /// <param name="key">They key to check for</param>
+        public abstract bool IsDown(EKeyboardCode key);
+
         /// <summary>
         /// Returns true if the systems current keyboard device has the given <paramref name="key"/> on it
         /// </summary>
