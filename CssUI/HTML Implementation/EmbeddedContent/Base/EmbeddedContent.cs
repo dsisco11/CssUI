0c6ee0d945466eb641c572d9874d386ca6d950d4
Sisco <dsisco11@gmail.com>
2019-08-16 00:15:51 -0500

    Create EmbeddedContent.cs
    Base class for any replaced/embedded content types

---
+++ CssUI/HTML Implementation/EmbeddedContent/Base/EmbeddedContent.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CssUI.HTML
+{
+    public abstract class EmbeddedContent
+    {/* Docs: https://html.spec.whatwg.org/multipage/dom.html#embedded-content-category */
+
+        internal int? Intrinsic_Width { get; }
+        internal int? Intrinsic_Height { get; }
+    }
+}
