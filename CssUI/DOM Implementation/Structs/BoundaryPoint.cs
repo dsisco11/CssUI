b92fab183d6f0489c2039e72b884eaf201d8defb
Sisco <dsisco11@gmail.com>
2019-08-16 00:14:10 -0500

    Update BoundaryPoint.cs
    Add comparison logic and constructor

--- CssUI/DOM Implementation/Structs/BoundaryPoint.cs
+++ CssUI/DOM Implementation/Structs/BoundaryPoint.cs
@@ -4,7 +4,43 @@ namespace CssUI.DOM
 {
     public struct BoundaryPoint
     {
+        #region Properties
         public Node node;
         public int offset;
+        #endregion
+
+        #region Constructors
+        public BoundaryPoint(Node node, int offset)
+        {
+            this.node = node;
+            this.offset = offset;
+        }
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            if (obj is BoundaryPoint other)
+            {
+                return ReferenceEquals(node, other.node) && offset == other.offset;
+            }
+
+            return base.Equals(obj);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(BoundaryPoint A, BoundaryPoint B)
+        {
+            return ReferenceEquals(A.node, B.node) && A.offset == B.offset;
+        }
+
+        public static bool operator !=(BoundaryPoint A, BoundaryPoint B)
+        {
+            return !ReferenceEquals(A.node, B.node) || A.offset != B.offset;
+        }
     }
 }
