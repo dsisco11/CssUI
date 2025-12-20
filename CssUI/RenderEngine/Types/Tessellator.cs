a8990397dbdeead27d6a3c5ff482b5af0d887cd5
David Sisco <dsisco11@gmail.com>
2025-12-20 10:48:00 -0800

    initial basic ITessellator implementation

---
+++ CssUI/RenderEngine/Types/Tessellator.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CssUI.Common;
+
+namespace CssUI;
+
+/// <summary>
+/// Default implementation of <see cref="ITessellator"/> that orchestrates
+/// tessellation of objects into mesh data.
+/// </summary>
+public sealed class Tessellator : ITessellator
+{
+    /// <inheritdoc/>
+    public ITessellationBuilder CreateBuilder(TessellationRequirements requirements)
+    {
+        return new TessellationBuilder(requirements);
+    }
+
+    /// <inheritdoc/>
+    public TessellationResult Tessellate(ITessellatable source)
+    {
+        var builder = CreateBuilder(source.Requirements);
+        source.Tessellate(builder);
+        return builder.Build();
+    }
+
+    /// <inheritdoc/>
+    public TessellationResult TessellateBatch(IEnumerable<ITessellatable> sources)
+    {
+        var sourceList = sources as IList<ITessellatable> ?? sources.ToList();
+
+        if (sourceList.Count == 0)
+        {
+            return TessellationResult.Empty;
+        }
+
+        // Combine requirements from all sources
+        var combinedRequirements = TessellationRequirements.None;
+        foreach (var source in sourceList)
+        {
+            combinedRequirements |= source.Requirements;
+        }
+
+        var builder = CreateBuilder(combinedRequirements);
+
+        // Tessellate each source into the shared builder
+        foreach (var source in sourceList)
+        {
+            source.Tessellate(builder);
+        }
+
+        return builder.Build();
+    }
+
+    /// <inheritdoc/>
+    public async ValueTask<MeshHandle> CreateMesh(TessellationResult result, IMeshService meshService)
+    {
+        if (result.IsEmpty)
+        {
+            return MeshHandle.Null;
+        }
+
+        if (result.IsIndexed)
+        {
+            return await meshService.CreateMesh(
+                result.Vertices,
+                result.Layout,
+                result.Indices,
+                result.IndexFormat);
+        }
+        else
+        {
+            return await meshService.CreateMesh(
+                result.Vertices,
+                result.Layout);
+        }
+    }
+}
